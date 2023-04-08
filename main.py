912b418a2fb6f747de01d3bb25412a73a3626c22
CelestialChewi <pheikhoon@gmail.com>
2023-04-08 20:42:37 +0800

    Fixed the AttributeError: 'Turtle' object has no attribute 'create_car by using "vehicle" instead of "car".

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from player import Player
 from car import Car
 from scoreboard import Scoreboard
@@ -10,7 +10,8 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car = Car()
+vehicle = Car()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
@@ -20,10 +21,22 @@ while game_on:
     time.sleep(0.1)
     screen.update()
 
-    car.create_car()
-    car.move_car()
+    vehicle.create_car()
+    vehicle.move_car()
 
     # Detect collision with cars
-    for car in car.all_car:
-        if car.distance(player) < 20:
+    for car in vehicle.all_cars:
+        if car.distance(player) < 24:
             game_on = False
+            scoreboard.screen_game_over()
+
+    # Detect when turtle reaches the other side
+    if player.at_finish_line():
+        player.start_again()
+        vehicle.level_up()
+        scoreboard.level_up_scoreboard()
+
+
+
+
+screen.exitonclick()
