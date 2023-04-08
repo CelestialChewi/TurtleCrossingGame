fb71441c78de4b824cf142bb66242aef05ae674a
CelestialChewi <pheikhoon@gmail.com>
2023-04-08 20:39:29 +0800

    Created function to increase car's speed after leveling up

--- car.py
+++ car.py
@@ -3,24 +3,29 @@ import random
 
 COLOURS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+INCREASE_MOVE = 10
 
 class Car:
 
     def __init__(self):
-        self.all_car = []
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
-        frequency = random.randint(1, 6)
-        if frequency == 1:  # Reduce the number of cars created in each 0.1 seconds
-            new_car = Turtle("square")
+        frequency = random.randint(1, 5)
+        if frequency == 1:
+            new_car = Turtle('square')
             new_car.shapesize(stretch_wid=1, stretch_len=2)
             new_car.penup()
             new_car.color(random.choice(COLOURS))
-            random_y = random.randint(-245, 245)
-            new_car.goto(295, random_y)
-            self.all_car.append(new_car)
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
 
     def move_car(self):
-        for car in self.all_car:
-            car.backward(STARTING_MOVE_DISTANCE)
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += INCREASE_MOVE
+
