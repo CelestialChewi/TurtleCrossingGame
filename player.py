b9a3912569d4ec53a2b78872ffd1a08716122a7a
CelestialChewi <pheikhoon@gmail.com>
2023-04-08 20:40:40 +0800

    Created function to detect the player at finishing line and reposition the player at starting line

--- player.py
+++ player.py
@@ -10,10 +10,17 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.start_again()
         self.setheading(90)
 
     def move_up(self):
         self.forward(MOVE_DISTANCE)
 
+    def at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
 
+    def start_again(self):
+        self.goto(STARTING_POSITION)
